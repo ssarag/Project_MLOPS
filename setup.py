8e86fd346da204a6f31dbf18edbb1c5ebfa0219d
ssarag <shalakasarag95@gmail.com>
2024-02-03 00:44:33 +0530

    exception logger and utils added

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
+
 __version__ = "0.0.0"
 
 REPO_NAME = "Project_MLOPS"
 AUTHOR_USER_NAME = "ssarag"
 SRC_REPO = "mlProject"
 AUTHOR_EMAIL = "shalakasarag95@gmail.com"
+
 
 setuptools.setup(
     name=SRC_REPO,
@@ -17,7 +19,7 @@ setuptools.setup(
     author_email=AUTHOR_EMAIL,
     description="A small python package for ml app",
     long_description=long_description,
-    long_description_content_type="text/markdown",  # Corrected argument
+    long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
---
+++ src/mlProject/components/__init__.py
@@ -0,0 +1,1 @@
+D:\MLOPS\Project_MLOPS\src\mlProject
